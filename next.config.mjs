d67a22c1770d0d52256864404c19750b5a26e51d
Divyanshu Kaushik <divyanshukaushik44@gmail.com>
2026-01-03 15:38:50 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'mongoose']
-    }
-    return config
-  },
-}
+    turbopack: {
+      
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals = [...(config.externals || []), "mongoose"];
+        }
+        return config;
+    },
+};
 
-export default nextConfig
+export default nextConfig;
